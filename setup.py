ddd7df2ca995ddf191b24c4d35e9dd28186e4535
Kyle Walker <KyleJamesWalker@gmail.com>
2019-04-04 14:53:56 -0700

    Release 2.0.1

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ requirements.update(all=sorted(set().union(*requirements.values())))
 
 setup(
     name='yamlsettings',
-    version='2.0.0',
+    version='2.0.1',
     description='Yaml Settings Configuration Module',
     long_description=readme,
     author='Kyle James Walker',
